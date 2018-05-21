3af85f44363154fe00615ead04a47d973c951337
Artyr <pavlechko777@gmail.com>
2018-05-21 16:19:34 +0300

    1) Add new style theme 2) Add new component today-movie

---
+++ src/app/components/home/today-movie/today-movie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-today-movie',
+  templateUrl: './today-movie.component.html',
+  styleUrls: ['./today-movie.component.css']
+})
+export class TodayMovieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
